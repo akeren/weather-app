f80fe9cca35c4d25f68ec84b4b5175ffe07b9baf
Kater Akeren <akerenkater@gmail.com>
2020-03-19 08:58:42 +0100

    Created a Darksky get wheather function

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
 
-const url =
+const darkskyUrl =
   'https://api.darksky.net/forecast/29264f98e04d8987d6fec747080e51c5/37.8267,-122.4233';
 
-const getWeather = async () => {
+const getWeather = async url => {
   try {
     const response = await axios.get(url);
-    console.log(response.data.currently);
+    //console.log(response);
+    const temperature = response.data.currently.temperature;
+    const precipProbability = response.data.currently.precipProbability;
+    const dailySummary = response.data.daily.data[0].summary;
+    console.log(
+      `${dailySummary} It's currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`
+    );
   } catch (error) {
     console.log(error);
   }
 };
-getWeather(); //calling the wheather function
+/*
+ ** calling the function
+ */
+getWeather(darkskyUrl);
