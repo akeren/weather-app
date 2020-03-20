e80b83091b954979fcc7d8e8ec7dfe7827029789
Kater Akeren <akerenkater@gmail.com>
2020-03-20 05:40:14 +0100

    Chaining the forcast and geocode

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const getGeocode = require('./utils/geocode');
 const getWheatherForcast = require('./utils/forcast');
 
-getWheatherForcast(8.53611, 7.73056, (error, response) => {
-  console.error('Error', error);
-  console.log('Data', response);
-});
-
 getGeocode('Makurdi', (error, response) => {
-  console.error('Error:', error);
-  console.log('Data:', response);
+  if (error) {
+    return console.error(error);
+  }
+  console.log(response.location);
+  getWheatherForcast(
+    response.longitude,
+    response.latitude,
+    (error, forcastData) => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log(forcastData.message);
+    }
+  );
 });
