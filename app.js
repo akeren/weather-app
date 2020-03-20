b921f4043f6423fc49f416a74d00b1be493bb6cd
Kater Akeren <akerenkater@gmail.com>
2020-03-20 03:05:48 +0100

    Handling geolocation errors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
-const darkskyURL =
+/* const darkskyURL =
   'https://api.darksky.net/forecast/29264f98e04d8987d6fec747080e51c5/37.8267,-122.4233';
 
 const getWeather = async url => {
@@ -15,25 +15,31 @@ const getWeather = async url => {
   } catch (error) {
     console.error('Unable to find wheather location.');
   }
-};
+}; */
 
-/* const geocodeURL =
+const geocodeURL =
   'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiYWtlcmVuIiwiYSI6ImNrN3lmNW81bzAxZnkzbXA5eDl6cmhkdjIifQ.QHY5oVZDX_gm_W_GDAw-EQ';
 
 const getGeocode = async url => {
   try {
     const response = await axios.get(url);
-    const features = response.data.features[0];
-    const longitude = features.center[0];
-    const latitude = features.center[1];
-    console.log(longitude, latitude);
+    if (response.data.features.length === 0) {
+      console.log('Unable to find the location. Try another search!');
+    } else {
+      const features = response.data.features[0];
+      const longitude = features.center[0];
+      const latitude = features.center[1];
+      console.log(longitude, latitude);
+    }
   } catch (error) {
-    console.log(error);
+    console.error(
+      'Unable to connect to location service. Check your network connectivity.'
+    );
   }
-}; */
+};
 
 /*
  ** calling the function
  */
-getWeather(darkskyURL);
-//getGeocode(geocodeURL);
+//getWeather(darkskyURL);
+getGeocode(geocodeURL);
