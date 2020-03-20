4f68aadc55dcad08b18c34b9feec6c591357cf14
Kater Akeren <akerenkater@gmail.com>
2020-03-20 05:46:01 +0100

    Providing input (address) via command line interface

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const getGeocode = require('./utils/geocode');
 const getWheatherForcast = require('./utils/forcast');
 
-getGeocode('Makurdi', (error, response) => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log(response.location);
-  getWheatherForcast(
-    response.longitude,
-    response.latitude,
-    (error, forcastData) => {
-      if (error) {
-        return console.error(error);
-      }
-      console.log(forcastData.message);
+const address = process.argv[2];
+if (!address) {
+  console.warn('Please provide an address!');
+} else {
+  getGeocode(address, (error, response) => {
+    if (error) {
+      return console.error(error);
     }
-  );
-});
+    console.log(response.location);
+    getWheatherForcast(
+      response.longitude,
+      response.latitude,
+      (error, forcastData) => {
+        if (error) {
+          return console.error(error);
+        }
+        console.log(forcastData.message);
+      }
+    );
+  });
+}
